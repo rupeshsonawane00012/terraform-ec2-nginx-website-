49c78ae0d0caa617da7f37b3917ceb91fed56151
Rupesh  Sonawane <148001273+rupeshsonawane00012@users.noreply.github.com>
2026-04-09 23:13:32 +0530

    Update user_data.sh

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -6,8 +6,8 @@ sudo apt install nginx -y
 echo "<html>
 <head><title>Rupesh DevOps Project</title></head>
 <body style='background:black;color:white;text-align:center;'>
-<h1>🚀 Hello from Terraform</h1>
-<h2>Deployed by Rupesh 🔥</h2>
+<h1> Hello from Terraform</h1>
+<h2>Deployed by Rupesh </h2>
 <p>This is my first DevOps project using AWS + Terraform</p>
 </body>
 </html>" > /var/www/html/index.html
